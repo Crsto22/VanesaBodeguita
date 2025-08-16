c3540b0cac5a0a1740064212a865ab41e8a562a2
crsto22 <arturo200512@gmail.com>
2025-08-16 15:06:59 -0500

    ultimo commit

--- vite.config.js
+++ vite.config.js
@@ -19,14 +19,14 @@ export default defineConfig({
       theme_color: '#ffffff',
       icons: [  
         {
-          src: '/assets/Logo.png',
+          src: '/icon-192x192.png',
           sizes: '192x192',
-          type: 'image/png+xml'
+          type: 'image/png'
         },
         {
-          src: '/assets/Logo.png',
+          src: '/icon-512x512.png',
           sizes: '512x512',
-          type: 'image/png+xml'
+          type: 'image/png'
         }
       ]
     }
